5a949bc8cbeb1206bff6faf9f3613f93bc53fd99
PolRivarola <rivarolapol@gmail.com>
2023-10-05 23:11:49 -0300

    conections-style

---
+++ src/pages/OferrerInterestees/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { OferrerInterestees } from '../../layouts/OferrerInterestees'
+
+export const OferrerInterestees = () => {
+  return (
+    <OferrerInterestees/>
+  )
+}
